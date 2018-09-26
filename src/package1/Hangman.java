de82bcdc0892a697683a90f08ee75462d91b7530
Michael Lannon <sMichaelLannon@yccc.edu>
2018-09-26 19:40:15 -0400

    first

---
+++ src/package1/Hangman.java
@@ -0,0 +1,56 @@
+package package1;
+//Hangman PSEUDOCODE
+import java.util.Scanner;
+public class Hangman {
+// Present game state (Mask = replace chars of word no guesses with asterisk)
+
+
+
+
+	private static String[] words = {"dog", "cat", "java", "cow", "window", "berlin" };
+	
+
+	
+
+	public static int x = 5;
+	
+public static int NUMBER_OF_BAD_GUESSES_BEFORE_DEATH = (x);
+public static void main(String args[]) 
+
+
+	
+	{
+		System.out.println("you have this many trys left:");
+System.out.println(NUMBER_OF_BAD_GUESSES_BEFORE_DEATH);
+
+
+//If not solved
+//get imput from user
+Scanner in = new Scanner(System.in);
+
+{
+    char [] c;
+    String temp;
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Please guess any letter");
+    //Prompt user to guess letter
+    temp=keyboard.next();
+    c=temp.toCharArray();
+     
+    }
+
+//Apply guess to game
+
+	}
+
+
+	
+ 
+ // Present game state (Mask = replace chars of word no guesses with asterisk)
+
+
+
+	
+	
+
+} 
