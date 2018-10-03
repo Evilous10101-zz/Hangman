1ea77b2ad595088653f59c0b4aa3e99cf3966f51
Michael Lannon <sMichaelLannon@yccc.edu>
2018-10-03 18:46:24 -0400

    save

--- src/package1/Hangman.java
+++ src/package1/Hangman.java
@@ -1,56 +1,79 @@
 package package1;
 //Hangman PSEUDOCODE
 import java.util.Scanner;
+import java.util.*;
 public class Hangman {
 // Present game state (Mask = replace chars of word no guesses with asterisk)
-
-
-
-
+	
+	private  String cat;
+    private  String word = cat;
+    public String mask;
+    
+	
+		
 	
 	
-
-	public  static int x = 5;
-	
-public  static int NUMBER_OF_BAD_GUESSES_BEFORE_DEATH = (x);
-public static void main(String args[]) {
-System.out.println("Hello! You are playing hangman please hold while we gather some data");
+	public static int x = 5;
+	 
+public static int NUMBER_OF_BAD_GUESSES_BEFORE_DEATH = (x);
+public static void main(String args[])
+ {
+System.out.println("Hello! You are playing hangman please hold while we gather some data. Please only use lowercase letters.");
 
 	{
 		
-		System.out.println("you have this many letters you can get wrong before you lose: " +  NUMBER_OF_BAD_GUESSES_BEFORE_DEATH);
-
+		System.out.println("You have this many letters you can get wrong before you lose: " + NUMBER_OF_BAD_GUESSES_BEFORE_DEATH);
+		
+	}
 
 //If not solved
+	
 //get input from user
-Scanner in = new Scanner(System.in);
-
+ }
+		Scanner in = new Scanner(System.in);
+		public void hangman()
 {
+	
+	
+	//Prompt user to guess letter
     char [] c;
     String temp;
     Scanner bee = new Scanner(System.in);
     System.out.println("Please guess any letter");
-    //Prompt user to guess letter
     temp=bee.next();
-    c=temp.toCharArray(); } 
-}
+    c=temp.toCharArray();
+    
+    if(NUMBER_OF_BAD_GUESSES_BEFORE_DEATH > 0) {
+    	
+    System.out.println("I am sorry you got that letter wrong. You have this many guesses left: " + NUMBER_OF_BAD_GUESSES_BEFORE_DEATH);
+    break; hangman();
+   
+    }
+    else {
+    	System.out.println("You beat the game, good job");
+    	 System.exit(0);
+    }
+     } 
+
+	
+ 
+
     
    
-    
-
 //Apply guess to game
 
-	}
+
 
  
  // Present game state (Mask = replace chars of word no guesses with asterisk)
 
+
 public void mark()
 {
-
-	System.out.println(NUMBER_OF_BAD_GUESSES_BEFORE_DEATH);
+	System.out.println(word);
 	
-	
-
-} 
 }
+}
+
+ 
+
